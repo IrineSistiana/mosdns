cd79c31041dd3091d0eedc5c1bf59d54b6690017
IrineSistiana <49315432+IrineSistiana@users.noreply.github.com>
2020-11-29 16:42:02 +0800

    handler: add args test

---
+++ dispatcher/handler/args_test.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"reflect"
+	"testing"
+)
+
+type TestArgsStruct struct {
+	A string `yaml:"1"`
+	B []int  `yaml:"2"`
+}
+
+func TestArgs_WeakDecode(t *testing.T) {
+	testObj := new(TestArgsStruct)
+	testArgs := Args{
+		"1": "test",
+		"2": []int{1, 2, 3},
+	}
+	wantObj := &TestArgsStruct{
+		A: "test",
+		B: []int{1, 2, 3},
+	}
+
+	err := testArgs.WeakDecode(testObj)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(testObj, wantObj) {
+		t.Fatalf("args decode failed, want %v, got %v", wantObj, testObj)
+	}
+}
