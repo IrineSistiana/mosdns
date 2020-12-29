cce303d2121723ff244b561ec3cb4161beb4eef6
IrineSistiana <49315432+IrineSistiana@users.noreply.github.com>
2020-12-29 22:03:39 +0800

    blackhole: block with ip

--- dispatcher/plugin/executable/blackhole/blackhole.go
+++ dispatcher/plugin/executable/blackhole/blackhole.go
@@ -19,8 +19,10 @@ package blackhole
 
 import (
 	"context"
+	"fmt"
 	"github.com/IrineSistiana/mosdns/dispatcher/handler"
 	"github.com/miekg/dns"
+	"net"
 )
 
 const PluginType = "blackhole"
@@ -28,19 +30,23 @@ const PluginType = "blackhole"
 func init() {
 	handler.RegInitFunc(PluginType, Init)
 
-	handler.MustRegPlugin(handler.WrapExecutablePlugin("_drop_response", PluginType, &blackhole{rCode: -1}))
-	handler.MustRegPlugin(handler.WrapExecutablePlugin("_block_with_servfail", PluginType, &blackhole{rCode: dns.RcodeServerFailure}))
-	handler.MustRegPlugin(handler.WrapExecutablePlugin("_block_with_nxdomain", PluginType, &blackhole{rCode: dns.RcodeNameError}))
+	handler.MustRegPlugin(handler.WrapExecutablePlugin("_drop_response", PluginType, &blackhole{args: &Args{RCode: -1}}))
+	handler.MustRegPlugin(handler.WrapExecutablePlugin("_block_with_servfail", PluginType, &blackhole{args: &Args{RCode: dns.RcodeServerFailure}}))
+	handler.MustRegPlugin(handler.WrapExecutablePlugin("_block_with_nxdomain", PluginType, &blackhole{args: &Args{RCode: dns.RcodeNameError}}))
 }
 
 var _ handler.Executable = (*blackhole)(nil)
 
 type blackhole struct {
-	rCode int
+	args *Args
+	ipv4 net.IP
+	ipv6 net.IP
 }
 
 type Args struct {
-	RCode int `yaml:"rcode"`
+	IPv4  string `yaml:"ipv4"` // block by responding specific IP
+	IPv6  string `yaml:"ipv6"`
+	RCode int    `yaml:"rcode"` // block by responding specific RCode
 }
 
 func Init(tag string, argsMap map[string]interface{}) (p handler.Plugin, err error) {
@@ -50,25 +56,75 @@ func Init(tag string, argsMap map[string]interface{}) (p handler.Plugin, err err
 		return nil, handler.NewErrFromTemplate(handler.ETInvalidArgs, err)
 	}
 
-	b := new(blackhole)
-	b.rCode = args.RCode
-
+	b, err := newBlackhole(tag, args)
+	if err != nil {
+		return nil, err
+	}
 	return handler.WrapExecutablePlugin(tag, PluginType, b), nil
 }
 
-// Do drops or replaces qCtx.R with a simple denial response.
+func newBlackhole(tag string, args *Args) (*blackhole, error) {
+	b := &blackhole{args: args}
+	if len(args.IPv4) != 0 {
+		ip := net.ParseIP(args.IPv4)
+		if ip == nil {
+			return nil, fmt.Errorf("%s is an invalid ipv4 addr", args.IPv4)
+		}
+		b.ipv4 = ip
+	}
+	if len(args.IPv6) != 0 {
+		ip := net.ParseIP(args.IPv6)
+		if ip == nil {
+			return nil, fmt.Errorf("%s is an invalid ipv6 addr", args.IPv6)
+		}
+		b.ipv6 = ip
+	}
+	return b, nil
+}
+
+// Exec
+// sets qCtx.R with IP response if query type is A/AAAA and Args.IPv4 / Args.IPv6 is not empty.
+// sets qCtx.R with empty response with rcode = Args.RCode.
+// drops qCtx.R if Args.RCode < 0
 // It never returns an err.
 func (b *blackhole) Exec(_ context.Context, qCtx *handler.Context) (err error) {
-	if qCtx == nil || qCtx.Q == nil {
-		return nil
-	}
-
 	switch {
-	case b.rCode >= 0:
+	case b.ipv4 != nil && len(qCtx.Q.Question) == 1 && qCtx.Q.Question[0].Qtype == dns.TypeA:
 		r := new(dns.Msg)
 		r.SetReply(qCtx.Q)
-		r.Rcode = b.rCode
+		rr := &dns.A{
+			Hdr: dns.RR_Header{
+				Name:   qCtx.Q.Question[0].Name,
+				Rrtype: dns.TypeA,
+				Class:  dns.ClassINET,
+				Ttl:    3600,
+			},
+			A: b.ipv4,
+		}
+		r.Answer = append(r.Answer, rr)
 		qCtx.SetResponse(r, handler.ContextStatusRejected)
+
+	case b.ipv6 != nil && len(qCtx.Q.Question) == 1 && qCtx.Q.Question[0].Qtype == dns.TypeAAAA:
+		r := new(dns.Msg)
+		r.SetReply(qCtx.Q)
+		rr := &dns.AAAA{
+			Hdr: dns.RR_Header{
+				Name:   qCtx.Q.Question[0].Name,
+				Rrtype: dns.TypeA,
+				Class:  dns.ClassINET,
+				Ttl:    3600,
+			},
+			AAAA: b.ipv6,
+		}
+		r.Answer = append(r.Answer, rr)
+		qCtx.SetResponse(r, handler.ContextStatusRejected)
+
+	case b.args.RCode >= 0:
+		r := new(dns.Msg)
+		r.SetReply(qCtx.Q)
+		r.Rcode = b.args.RCode
+		qCtx.SetResponse(r, handler.ContextStatusRejected)
+
 	default:
 		qCtx.SetResponse(nil, handler.ContextStatusDropped)
 	}
