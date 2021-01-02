1a004345f5dd08656a9a56b48bd59f163df9b9f9
IrineSistiana <49315432+IrineSistiana@users.noreply.github.com>
2021-01-02 19:42:35 +0800

    handler: fixed test

--- dispatcher/handler/executable_plugin_test.go
+++ dispatcher/handler/executable_plugin_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"github.com/IrineSistiana/mosdns/dispatcher/mlog"
+	"gopkg.in/yaml.v3"
 	"testing"
 )
 
@@ -11,100 +12,70 @@ func Test_switchPlugin_Do(t *testing.T) {
 	PurgePluginRegister()
 	defer PurgePluginRegister()
 
-	mErr := errors.New("")
-	eErr := errors.New("")
+	mErr := errors.New("mErr")
+	eErr := errors.New("eErr")
 
-	notMatched := "not_matched"
-	matched := "matched"
-	matchErr := "match_err"
-
-	exec := executablePluginTag("exec")
-	execErr := executablePluginTag("exec_err")
 	type args struct {
 		executable ExecutableCmd
 	}
 
 	var tests = []struct {
 		name     string
-		args     *args
+		yamlStr  string
 		wantNext string
 		wantErr  error
 	}{
-		{name: "try to reach empty end", args: &args{
-			executable: &ExecutableCmdSequence{exec, exec,
-				&ifBlock{
-					ifMatcher:     []string{"!" + matched, notMatched}, // not matched
-					executableCmd: &ExecutableCmdSequence{execErr},
-					goTwo:         "goto",
-				},
-			},
-		}, wantNext: "", wantErr: nil},
+		{name: "test empty end", yamlStr: `
+exec:
+- if: ["!matched",not_matched] # not matched
+  exec: [exec_err]
+  goto: goto`,
+			wantNext: "", wantErr: nil},
 
-		{name: "test if_and", args: &args{
-			executable: &ExecutableCmdSequence{exec, exec,
-				&ifBlock{
-					ifAndMatcher: []string{matched, notMatched}, // not matched
-					goTwo:        "goto1",
-				},
-				&ifBlock{
-					ifAndMatcher: []string{matched, notMatched, matchErr}, // not matched, early stop, no err
-					goTwo:        "goto2",
-				},
-				&ifBlock{
-					ifAndMatcher: []string{matched, matched, matched}, // matched
-					goTwo:        "goto3",
-				},
-			},
-		}, wantNext: "goto3", wantErr: nil},
+		{name: "test if_and", yamlStr: `
+exec:
+- if_and: [matched, not_matched] # not matched
+  goto: goto1
+- if_and: [matched, not_matched, match_err] # not matched, early stop, no err
+  goto: goto2
+- if_and: [matched, matched, matched] # matched
+  goto: goto3
+`,
+			wantNext: "goto3", wantErr: nil},
 
-		{name: "test if_and err", args: &args{
-			executable: &ExecutableCmdSequence{exec, exec,
-				&ifBlock{
-					ifAndMatcher: []string{matched, matchErr}, // err
-					goTwo:        "goto1",
-				},
-			},
-		}, wantNext: "", wantErr: mErr},
+		{name: "test if_and err", yamlStr: `
+exec:
+- if_and: [matched, match_err] # err
+  goto: goto1
+`,
+			wantNext: "", wantErr: mErr},
 
-		{name: "test if", args: &args{
-			executable: &ExecutableCmdSequence{exec, exec,
-				&ifBlock{
-					ifMatcher: []string{"!" + matched, notMatched}, // test ! prefix, not matched
-					goTwo:     "goto1",
-				},
-				&ifBlock{
-					ifMatcher: []string{matched, matchErr}, // matched, early stop, no err.
-					executableCmd: &ExecutableCmdSequence{
-						exec,
-						&ifBlock{
-							ifMatcher: []string{"!" + notMatched, matchErr}, // // test ! prefix, matched, early stop, no err.
-							goTwo:     "goto2",                              // reached here
-						},
-					},
-					goTwo: "goto3",
-				},
-			},
-		}, wantNext: "goto2", wantErr: nil},
+		{name: "test if", yamlStr: `
+exec:
+- if: ["!matched", not_matched] # test ! prefix, not matched
+  goto: goto1
+- if: [matched, match_err] # matched, early stop, no err
+  exec:
+  - if: ["!not_matched", not_matched] # matched
+    goto: goto2 						# reached here
+  goto: goto3
+`,
+			wantNext: "goto2", wantErr: nil},
 
-		{name: "test if matcher err", args: &args{
-			executable: &ExecutableCmdSequence{exec, exec,
-				&ifBlock{
-					ifMatcher:     []string{"!" + matched, notMatched, matchErr}, // matcher err
-					executableCmd: &ExecutableCmdSequence{exec},
-					goTwo:         "goto",
-				},
-			},
-		}, wantNext: "", wantErr: mErr},
+		{name: "test if err", yamlStr: `
+exec:
+- if: [not_matched, match_err] # err
+  goto: goto1
+`,
+			wantNext: "", wantErr: mErr},
 
-		{name: "test exec err", args: &args{
-			executable: &ExecutableCmdSequence{exec, exec,
-				&ifBlock{
-					ifMatcher:     []string{"!" + matched, matched},
-					executableCmd: &ExecutableCmdSequence{execErr},
-					goTwo:         "goto",
-				},
-			},
-		}, wantNext: "", wantErr: eErr},
+		{name: "test exec err", yamlStr: `
+exec:
+- if: [matched] 
+  exec: exec_err
+  goto: goto1
+`,
+			wantNext: "", wantErr: eErr},
 	}
 
 	mustSuccess := func(err error) {
@@ -113,32 +84,43 @@ func Test_switchPlugin_Do(t *testing.T) {
 		}
 	}
 
-	// notMatched
-	mustSuccess(RegPlugin(WrapMatcherPlugin(notMatched, "",
+	// not_matched
+	mustSuccess(RegPlugin(WrapMatcherPlugin("not_matched", "",
 		&DummyMatcher{Matched: false, WantErr: nil},
 	)))
 
 	// do something
-	mustSuccess(RegPlugin(WrapExecutablePlugin(string(exec), "",
+	mustSuccess(RegPlugin(WrapExecutablePlugin("exec", "",
 		&DummyExecutable{WantErr: nil},
 	)))
 
 	// matched
-	mustSuccess(RegPlugin(WrapMatcherPlugin(matched, "",
+	mustSuccess(RegPlugin(WrapMatcherPlugin("matched", "",
 		&DummyMatcher{Matched: true, WantErr: nil},
 	)))
 
 	// plugins should return an err.
-	mustSuccess(RegPlugin(WrapMatcherPlugin(matchErr, "",
+	mustSuccess(RegPlugin(WrapMatcherPlugin("match_err", "",
 		&DummyMatcher{Matched: false, WantErr: mErr},
 	)))
-	mustSuccess(RegPlugin(WrapExecutablePlugin(string(execErr), "",
+	mustSuccess(RegPlugin(WrapExecutablePlugin(string("exec_err"), "",
 		&DummyExecutable{WantErr: eErr},
 	)))
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotNext, err := tt.args.executable.ExecCmd(context.Background(), nil, mlog.NewPluginLogger("test"))
+			args := make(map[string]interface{}, 0)
+			err := yaml.Unmarshal([]byte(tt.yamlStr), args)
+			if err != nil {
+				t.Fatal(err)
+			}
+			ecs := NewExecutableCmdSequence()
+			err = ecs.Parse(args["exec"].([]interface{}))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			gotNext, err := ecs.ExecCmd(context.Background(), nil, mlog.NewPluginLogger("test"))
 			if (err != nil || tt.wantErr != nil) && !errors.Is(err, tt.wantErr) {
 				t.Errorf("Do() error = %v, wantErr %v", err, tt.wantErr)
 				return
