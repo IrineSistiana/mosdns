9b956b09685632169237cdecb98c70f363e452be
mark-syctnet <81214737+mark-syctnet@users.noreply.github.com>
2022-07-04 15:16:44 +0800

    Create iptoshell_other.go

---
+++ plugin/executable/iptoshell/iptoshell_other.go
@@ -0,0 +1,41 @@
+//go:build !linux
+
+/*
+ * Copyright (C) 2020-2022, IrineSistiana
+ *
+ * This file is part of mosdns.
+ *
+ * mosdns is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mosdns is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package iptoshell
+
+import (
+	"context"
+	"github.com/IrineSistiana/mosdns/v4/coremain"
+	"github.com/IrineSistiana/mosdns/v4/pkg/executable_seq"
+	"github.com/IrineSistiana/mosdns/v4/pkg/query_context"
+)
+
+type iptoshellPlugin struct {
+	*coremain.BP
+}
+
+func newiptoshellPlugin(bp *coremain.BP, args *Args) (*iptoshellPlugin, error) {
+	return &iptoshellPlugin{BP: bp}, nil
+}
+
+func (p *iptoshellPlugin) Exec(ctx context.Context, qCtx *query_context.Context, next executable_seq.ExecutableChainNode) error {
+	return executable_seq.ExecChainNode(ctx, qCtx, next)
+}
