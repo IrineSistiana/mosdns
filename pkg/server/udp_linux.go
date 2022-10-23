73e8172f80ec64e4dba4e232f0ea5e4dff4c44c8
Urle Sistiana <55231606+urlesistiana@users.noreply.github.com>
2022-10-23 21:14:28 +0800

    server: fix invalid argument in sendmsg

--- pkg/server/udp_linux.go
+++ pkg/server/udp_linux.go
@@ -47,19 +47,37 @@ func (i ipv4PacketConn) readFrom(b []byte) (n int, cm any, src net.Addr, err err
 }
 
 func (i ipv4PacketConn) writeTo(b []byte, cm any, dst net.Addr) (n int, err error) {
-	return i.c.WriteTo(b, cm.(*ipv4.ControlMessage), dst)
+	cm4 := cm.(*ipv4.ControlMessage)
+	cm4.Src = cm4.Dst
+	cm4.Dst = nil
+	return i.c.WriteTo(b, cm4, dst)
 }
 
 type ipv6PacketConn struct {
-	c *ipv6.PacketConn
+	c4 *ipv4.PacketConn // ipv4 entrypoint for sending ipv4 packages.
+	c6 *ipv6.PacketConn
 }
 
 func (i ipv6PacketConn) readFrom(b []byte) (n int, cm any, src net.Addr, err error) {
-	return i.c.ReadFrom(b)
+	return i.c6.ReadFrom(b)
 }
 
 func (i ipv6PacketConn) writeTo(b []byte, cm any, dst net.Addr) (n int, err error) {
-	return i.c.WriteTo(b, cm.(*ipv6.ControlMessage), dst)
+	cm6 := cm.(*ipv6.ControlMessage)
+	cm6.Src = cm6.Dst
+	cm6.Dst = nil
+
+	// If src is ipv4, use IP_PKTINFO instead of IPV6_PKTINFO.
+	// Otherwise, sendmsg will raise "invalid argument" error.
+	// No official doc found.
+	if src4 := cm6.Src.To4(); src4 != nil {
+		return i.c4.WriteTo(b, &ipv4.ControlMessage{
+			Src:     src4,
+			IfIndex: cm6.IfIndex,
+		}, dst)
+	} else {
+		return i.c6.WriteTo(b, cm6, dst)
+	}
 }
 
 func newUDPConn(c *net.UDPConn) (cmcUDPConn, error) {
@@ -70,16 +88,16 @@ func newUDPConn(c *net.UDPConn) (cmcUDPConn, error) {
 	switch p {
 	case v4:
 		c := ipv4.NewPacketConn(c)
-		if err := c.SetControlMessage(ipv4.FlagSrc|ipv4.FlagDst|ipv4.FlagInterface, true); err != nil {
+		if err := c.SetControlMessage(ipv4.FlagDst|ipv4.FlagInterface, true); err != nil {
 			return nil, fmt.Errorf("failed to set ipv4 cmsg flags, %w", err)
 		}
 		return ipv4PacketConn{c: c}, nil
 	case v6:
-		c := ipv6.NewPacketConn(c)
-		if err := c.SetControlMessage(ipv6.FlagSrc|ipv6.FlagDst|ipv6.FlagInterface, true); err != nil {
+		c6 := ipv6.NewPacketConn(c)
+		if err := c6.SetControlMessage(ipv6.FlagDst|ipv6.FlagInterface, true); err != nil {
 			return nil, fmt.Errorf("failed to set ipv6 cmsg flags, %w", err)
 		}
-		return ipv6PacketConn{c: c}, nil
+		return ipv6PacketConn{c6: c6, c4: ipv4.NewPacketConn(c)}, nil
 	default:
 		return nil, fmt.Errorf("unknow protocol %d", p)
 	}
