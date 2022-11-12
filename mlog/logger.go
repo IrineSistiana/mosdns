9692d5cbbbdc7777b9601cc4c24d1aa4f2e695ce
Urle Sistiana <55231606+urlesistiana@users.noreply.github.com>
2022-11-12 21:10:55 +0800

    mlog: add options to omit time and output log in json

--- mlog/logger.go
+++ mlog/logger.go
@@ -27,83 +27,71 @@ import (
 )
 
 type LogConfig struct {
-	Level    string `yaml:"level"`
-	File     string `yaml:"file"`
-	ErrFile  string `yaml:"err_file"`
-	InfoFile string `yaml:"info_file"`
+	// Level, See also zapcore.ParseLevel.
+	Level string `yaml:"level"`
+
+	// File that logger will be writen into.
+	// Default is stderr.
+	File string `yaml:"file"`
+
+	// Production enables json output.
+	Production bool `yaml:"production"`
+
+	// OmitTime omits the time in log.
+	OmitTime bool `yaml:"omit_time"`
+
+	// parsed level
+	lvl zapcore.Level
 }
 
 var (
-	stdout = zapcore.Lock(os.Stdout)
 	stderr = zapcore.Lock(os.Stderr)
 
 	lvl = zap.NewAtomicLevelAt(zap.InfoLevel)
-	l   = newLogger(lvl, stdout, stderr)
+	l   = newLogger(zapcore.NewConsoleEncoder, defaultEncoderConfig(), lvl, stderr)
 	s   = l.Sugar()
 )
 
 func NewLogger(lc *LogConfig) (*zap.Logger, error) {
-	var lvl zapcore.Level
-	if len(lc.Level) > 0 {
-		var ok bool
-		lvl, ok = parseLogLevel(lc.Level)
-		if !ok {
-			return nil, fmt.Errorf("invalid log level [%s]", lc.Level)
-		}
+	lvl, err := zapcore.ParseLevel(lc.Level)
+	if err != nil {
+		return nil, fmt.Errorf("invalid log level: %w", err)
 	}
+	lc.lvl = lvl
 
-	var infoWriter zapcore.WriteSyncer
-	var errWriter zapcore.WriteSyncer
+	return newLoggerFromCfg(lc)
+}
 
+func newLoggerFromCfg(lc *LogConfig) (*zap.Logger, error) {
+	var out zapcore.WriteSyncer
 	if lf := lc.File; len(lf) > 0 {
 		f, _, err := zap.Open(lf)
 		if err != nil {
 			return nil, fmt.Errorf("open log file: %w", err)
 		}
-		fLocked := zapcore.Lock(f)
-		infoWriter = fLocked
-		errWriter = fLocked
+		out = zapcore.Lock(f)
 	} else {
-		if lf := lc.InfoFile; len(lf) > 0 {
-			f, _, err := zap.Open(lf)
-			if err != nil {
-				return nil, fmt.Errorf("open info log file: %w", err)
-			}
-			infoWriter = zapcore.Lock(f)
-		}
-		if lf := lc.ErrFile; len(lf) > 0 {
-			f, _, err := zap.Open(lf)
-			if err != nil {
-				return nil, fmt.Errorf("open err log file: %w", err)
-			}
-			errWriter = zapcore.Lock(f)
-		}
+		out = stderr
 	}
 
-	return newLogger(lvl, infoWriter, errWriter), nil
+	ec := defaultEncoderConfig()
+	if lc.OmitTime {
+		ec.TimeKey = ""
+	}
+
+	if lc.Production {
+		return newLogger(zapcore.NewJSONEncoder, ec, lc.lvl, out), nil
+	}
+	return newLogger(zapcore.NewConsoleEncoder, ec, lc.lvl, out), nil
 }
 
-// newLogger creates a new zap.Logger, by default the infoWriter and errWriter are
-// stdout and stderr.
-func newLogger(lvl zapcore.LevelEnabler, infoWriter, errWriter zapcore.WriteSyncer) *zap.Logger {
-	errLvl := zap.LevelEnablerFunc(func(l2 zapcore.Level) bool {
-		return lvl.Enabled(l2) && l2 >= zapcore.ErrorLevel
-	})
-	infoLvl := zap.LevelEnablerFunc(func(l2 zapcore.Level) bool {
-		return lvl.Enabled(l2) && l2 < zapcore.ErrorLevel
-	})
-
-	if infoWriter == nil {
-		infoWriter = stdout
-	}
-	if errWriter == nil {
-		errWriter = stderr
-	}
-
-	core := zapcore.NewTee(
-		zapcore.NewCore(zapcore.NewConsoleEncoder(defaultEncoderConfig()), infoWriter, infoLvl),
-		zapcore.NewCore(zapcore.NewConsoleEncoder(defaultEncoderConfig()), errWriter, errLvl),
-	)
+func newLogger(
+	encoderFactory func(config zapcore.EncoderConfig) zapcore.Encoder,
+	encoderCfg zapcore.EncoderConfig,
+	lvl zapcore.LevelEnabler,
+	out zapcore.WriteSyncer,
+) *zap.Logger {
+	core := zapcore.NewCore(encoderFactory(encoderCfg), out, lvl)
 	return zap.New(core)
 }
 
@@ -118,7 +106,6 @@ func SetLevel(l zapcore.Level) {
 func S() *zap.SugaredLogger {
 	return s
 }
-
 func defaultEncoderConfig() zapcore.EncoderConfig {
 	return zapcore.EncoderConfig{
 		TimeKey:        "time",
@@ -130,20 +117,5 @@ func defaultEncoderConfig() zapcore.EncoderConfig {
 		EncodeTime:     zapcore.ISO8601TimeEncoder,
 		EncodeDuration: zapcore.StringDurationEncoder,
 		EncodeCaller:   zapcore.ShortCallerEncoder,
-	}
-}
-
-func parseLogLevel(s string) (zapcore.Level, bool) {
-	switch s {
-	case "debug":
-		return zap.DebugLevel, true
-	case "", "info":
-		return zap.InfoLevel, true
-	case "warn":
-		return zap.WarnLevel, true
-	case "error":
-		return zap.ErrorLevel, true
-	default:
-		return 0, false
 	}
 }
