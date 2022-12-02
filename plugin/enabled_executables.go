37d7e083f065659982623948f2a47fdc001a0fd9
Urle Sistiana <55231606+urlesistiana@users.noreply.github.com>
2022-12-02 10:27:57 +0800

    remove misc_optm

--- plugin/enabled_executables.go
+++ plugin/enabled_executables.go
@@ -31,7 +31,6 @@ import (
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/hosts"
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/ipset"
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/metrics_collector"
-	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/misc_optm"
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/nftset"
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/query_summary"
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/redirect"
