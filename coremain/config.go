7856323b61be57648734e2fdf1078bbdbcaaf7ac
Urle Sistiana <55231606+urlesistiana@users.noreply.github.com>
2022-12-09 22:04:24 +0800

    remove unused code

--- coremain/config.go
+++ coremain/config.go
@@ -21,7 +21,6 @@ package coremain
 
 import (
 	"github.com/IrineSistiana/mosdns/v5/mlog"
-	"github.com/IrineSistiana/mosdns/v5/pkg/utils"
 )
 
 type Config struct {
@@ -45,56 +44,6 @@ type PluginConfig struct {
 	Args interface{} `yaml:"args"`
 }
 
-type ServerConfig struct {
-	Exec      string                  `yaml:"exec"`
-	Timeout   uint                    `yaml:"timeout"` // (sec) query timeout.
-	Listeners []*ServerListenerConfig `yaml:"listeners"`
-}
-
-type ServerListenerConfig struct {
-	// Protocol: server protocol, can be:
-	// "", "udp" -> udp
-	// "tcp" -> tcp
-	// "dot", "tls" -> dns over tls
-	// "doh", "https" -> dns over https (rfc 8844)
-	// "http" -> dns over https (rfc 8844) but without tls
-	Protocol string `yaml:"protocol"`
-
-	// Addr: server "host:port" addr.
-	// Addr cannot be empty.
-	Addr string `yaml:"addr"`
-
-	Cert                string `yaml:"cert"`                    // certificate path, used by dot, doh
-	Key                 string `yaml:"key"`                     // certificate key path, used by dot, doh
-	URLPath             string `yaml:"url_path"`                // used by doh, http. If it's empty, any path will be handled.
-	GetUserIPFromHeader string `yaml:"get_user_ip_from_header"` // used by doh, http.
-	ProxyProtocol       bool   `yaml:"proxy_protocol"`          // accepting the PROXYProtocol
-
-	IdleTimeout uint `yaml:"idle_timeout"` // (sec) used by tcp, dot, doh as connection idle timeout.
-}
-
 type APIConfig struct {
 	HTTP string `yaml:"http"`
-}
-
-type SecurityConfig struct {
-	BadIPObserver BadIPObserverConfig `yaml:"bad_ip_observer"`
-}
-
-// BadIPObserverConfig is a copy of ip_observer.BadIPObserverOpts.
-type BadIPObserverConfig struct {
-	Threshold        int    `yaml:"threshold"` // Zero Threshold will disable the bad ip observer.
-	Interval         int    `yaml:"interval"`  // (sec) Default is 10.
-	TTL              int    `yaml:"ttl"`       // (sec) Default is 600 (10min).
-	OnUpdateCallBack string `yaml:"on_update_callback"`
-	// IP masks to aggregate an IP range.
-	IPv4Mask int `yaml:"ipv4_mask"` // Default is 32.
-	IPv6Mask int `yaml:"ipv6_mask"` // Default is 48.
-}
-
-func (c *BadIPObserverConfig) Init() {
-	utils.SetDefaultNum(&c.Interval, 10)
-	utils.SetDefaultNum(&c.TTL, 600)
-	utils.SetDefaultNum(&c.IPv4Mask, 32)
-	utils.SetDefaultNum(&c.IPv6Mask, 48)
 }
